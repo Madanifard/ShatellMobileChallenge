61e90a068dbff0c5ae4964cb4e6c0da0e891b777
Amirreza Madanifard <amadanifard@gmail.com>
2024-05-09 12:34:07 +0330

    add drf and drf-jwt

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -44,12 +44,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # ###########################
+    'rest_framework',
     'user',
 ]
 
 
 #change the User Model
 AUTH_USER_MODEL = 'user.AdminUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
